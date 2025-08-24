40ed9c4bd2ad0ab4ea3896e12cd0b9f55884ce99
Oskar Grunning <oskargrunning@gmail.com>
2025-08-24 15:53:14 +0200

    test: add comprehensive coverage for convert file preservation
    Sets up global git config in test infrastructure to eliminate duplication.
    Adds extensive integration tests validating file preservation behavior
    including mixed tracked/untracked files and directory structures.

--- cmd/grove/script_test.go
+++ cmd/grove/script_test.go
@@ -3,6 +3,8 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/rogpeppe/go-internal/testscript"
@@ -13,6 +15,25 @@ func TestScript(t *testing.T) {
 		Dir: "testdata/script",
 		Setup: func(env *testscript.Env) error {
 			env.Vars = append(env.Vars, "GROVE_PLAIN=true")
+			homeDir := filepath.Join(env.WorkDir, ".home")
+			if err := os.MkdirAll(homeDir, 0755); err != nil {
+				return err
+			}
+			env.Vars = append(env.Vars, "HOME="+homeDir)
+			gitConfigPath := filepath.Join(homeDir, ".gitconfig")
+			gitConfigContent := `[init]
+	defaultBranch = main
+[advice]
+	defaultBranchName = false
+[user]
+	name = Test
+	email = test@example.com
+[commit]
+	gpgsign = false
+`
+			if err := os.WriteFile(gitConfigPath, []byte(gitConfigContent), 0644); err != nil {
+				return err
+			}
 			return nil
 		},
 	})
