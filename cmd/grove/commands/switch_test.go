72a8691a5ebd6f3a0286ab84278609adc22f8479
Oskar Grunning <oskargrunning@gmail.com>
2025-11-27 08:00:07 +0100

    test(switch): add unit tests for error handling

--- cmd/grove/commands/switch_test.go
+++ cmd/grove/commands/switch_test.go
@@ -1,7 +1,11 @@
 package commands
 
 import (
+	"errors"
+	"os"
 	"testing"
+
+	"github.com/sqve/grove/internal/workspace"
 )
 
 func TestNewSwitchCmd(t *testing.T) {
@@ -11,5 +15,23 @@ func TestNewSwitchCmd(t *testing.T) {
 	}
 	if cmd.Short == "" {
 		t.Error("expected Short description to be set")
+	}
+}
+
+func TestRunSwitch_NotInWorkspace(t *testing.T) {
+	origDir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() { _ = os.Chdir(origDir) }()
+
+	tmpDir := t.TempDir()
+	if err := os.Chdir(tmpDir); err != nil {
+		t.Fatal(err)
+	}
+
+	err = runSwitch("main")
+	if !errors.Is(err, workspace.ErrNotInWorkspace) {
+		t.Errorf("expected ErrNotInWorkspace, got %v", err)
 	}
 }
